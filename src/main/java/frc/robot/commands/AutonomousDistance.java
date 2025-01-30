ac990a05407f5108dc1795ce4c8c129abc5a1cc2
anteressssssss <aidenw@khanlabschool.org>
2025-01-29 19:29:37 -0800

    More work on drawing different shapes with the robot's path

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -14,11 +14,49 @@ public class AutonomousDistance extends SequentialCommandGroup {
    *
    * @param drivetrain The drivetrain subsystem on which this command will run
    */
-  public AutonomousDistance(Drivetrain drivetrain) {
-    addCommands(
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(-0.5, 180, drivetrain),
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(0.5, 180, drivetrain));
+  private void drawCircle(int radius, Drivetrain drivetrain){
+    double length = (Math.PI/180)*radius;
+    int degreesToTurnBy = 1;
+  
+    for(int i = 0; i < 360; i++){
+      addCommands(
+        new DriveDistance(0.5, length, drivetrain),
+        new TurnDegrees(0.5, degreesToTurnBy, drivetrain));
+    }
   }
-}
+  private void drawSquare(int sideLength, Drivetrain drivetrain){
+    int degreesToTurnBy = 90;
+    for(int i = 0; i < 4; i++){
+      addCommands(
+        new DriveDistance(1, sideLength, drivetrain),
+        new TurnDegrees(1, degreesToTurnBy, drivetrain));
+    }
+  }
+  private void drawTriangle(int sideLength, Drivetrain drivetrain){
+    int degreesToTurnBy = 120;
+    for (int i = 0; i < 3; i++){
+      addCommands(
+        new DriveDistance(1, sideLength, drivetrain),
+        new TurnDegrees(1, degreesToTurnBy, drivetrain));
+    }
+  }
+  private void whatever(int var, Drivetrain drivetrain){
+    for (int i = 0; i < 1000; i++){
+      addCommands(
+        new DriveDistance(1, var, drivetrain),
+        new TurnDegrees(1, 180, drivetrain));
+    }
+  }
+  
+  
+    /**
+     * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+     * turn around and drive back.
+     *
+     * @param drivetrain The drivetrain subsystem on which this command will run
+     */
+    public AutonomousDistance(Drivetrain drivetrain) {
+      drawCircle(100, drivetrain);
+    }
+  }
+  
